589ef130cf20c8562fef37507988bc9b7f097486
MykalB081 <mykalb081@gmail.com>
2021-03-11 16:20:52 -0500

    Update
    - Tutorial completed and web app created

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # Flask test
 
-In terminal, navigate to directory of hello.py and enter:
-```bash
-FLASK_APP=hello.py flask run
-```
+A simple Blog Web App
 
+Python version: 3.8.7  
+Flask version: 1.1.2
+SQLite version: 3.19.3
+
+## Purpose:
+Practice using Flask for future use.
+
+## Details
+The tutorial on [www.digitalocean.com](https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3) to create a web application.   
+
+### Usage:
+1. initiate database with the following command:  
+```bash
+python3 init_db.py
+```
+which will create 'database.db' in the directory.  The database will contain 'posts' for the blog and will be initialized with 2 posts.  
+
+2. In terminal, navigate to directory of app.py and enter:  
+```bash
+FLASK_APP=app.py flask run
+```
+3. In a web broswer, navigate to ```127.0.0.1:5000``` to access the web app.
---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+import sqlite3
+from werkzeug.exceptions import abort
+'''
+The global request object to access incoming request data that will be submitted via an HTML form.
+The url_for() function to generate URLs.
+The flash() function to flash a message when a request is processed.
+The redirect() function to redirect the client to a different location.
+'''
+
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'do not snoop my secret key'
+
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
+# GET requests are accepted by default
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
